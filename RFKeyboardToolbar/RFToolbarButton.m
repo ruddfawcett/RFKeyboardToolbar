a9aa4b45a761cc1cff324406813ab0c366f64bb0
Brandon Butler <hackmodford@gmail.com>
2014-01-06 11:20:35 -0500

    Update RFToolbarButton.m
    simplified code

--- RFKeyboardToolbar/RFToolbarButton.m
+++ RFKeyboardToolbar/RFToolbarButton.m
@@ -1,4 +1,4 @@
-//
+/
 //  RFToolbarButton.m
 //
 //  Created by Rex Finn on 12/3/13.
@@ -7,14 +7,26 @@
 
 #import "RFToolbarButton.h"
 
+@interface RFToolbarButton ()
+
+@property (nonatomic, strong) NSString *title;
+
+@end
+
 @implementation RFToolbarButton
 
-static UITextView *textView = NULL;
-static UITextField *textField = NULL;
++(instancetype)buttonWithTitle:(NSString *)title {
+    return [[self alloc] initWithTitle:title];
+}
+
+-(id)initWithTitle:(NSString *)title {
+    _title = title;
+    return [self init];
+}
 
 - (id)init
 {
-    CGSize sizeOfText = [[self titleForButton] sizeWithAttributes:@{NSFontAttributeName:[UIFont boldSystemFontOfSize:14.f]}];
+    CGSize sizeOfText = [self.title sizeWithAttributes:@{NSFontAttributeName:[UIFont boldSystemFontOfSize:14.f]}];
     
     self = [super initWithFrame:CGRectMake(0, 0, sizeOfText.width + 18.104, sizeOfText.height + 10.298)];
     if (self) {
@@ -24,37 +36,13 @@ static UITextField *textField = NULL;
         self.layer.borderWidth = 1.0f;
         self.layer.borderColor = [UIColor colorWithWhite:0.8 alpha:1.0].CGColor;
         
-        [self setTitle:[self titleForButton] forState:UIControlStateNormal];
+        [self setTitle:self.title forState:UIControlStateNormal];
         [self setTitleColor:[UIColor colorWithWhite:0.500 alpha:1.0] forState:UIControlStateNormal];
         
         self.titleLabel.font = [UIFont boldSystemFontOfSize:14.f];
         self.titleLabel.textColor = [UIColor colorWithWhite:0.500 alpha:1.0];
-        
-        [self addTarget:self action:@selector(buttonTarget) forControlEvents:UIControlEventTouchUpInside];
     }
     return self;
 }
-
-+ (void)setTextViewForButton:(UITextView*)textViewPassed {
-    textView = textViewPassed;
-}
-
-+ (UITextView*)textView {
-    return textView;
-}
-
-+ (void)setTextFieldForButton:(UITextField*)textFieldPassed {
-    textField = textFieldPassed;
-}
-
-+ (UITextField*)textField {
-    return textField;
-}
-
-- (NSString*)titleForButton {
-    return nil;
-}
-
--(void)buttonTarget {}
 
 @end
