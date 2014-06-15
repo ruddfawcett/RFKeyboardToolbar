61ec8a9f8f6e478ab0e375ab152f274da3f44ce9
Jesús A. Álvarez <zydeco@namedfork.net>
2014-06-15 18:30:33 +0200

    update example to include changing buttons

--- RFKeyboardToolbarDemo/RFKeyboardToolbar/ViewController.m
+++ RFKeyboardToolbarDemo/RFKeyboardToolbar/ViewController.m
@@ -17,6 +17,9 @@
 @end
 
 @implementation ViewController
+{
+    RFKeyboardToolbar *keyboardToolbar;
+}
 
 - (void)viewDidLoad
 {
@@ -32,9 +35,41 @@
         [_textView insertText:@"You pressed a button!"];
     } forControlEvents:UIControlEventTouchUpInside];
     
-    _textView.inputAccessoryView = [RFKeyboardToolbar toolbarViewWithButtons:@[exampleButton]];
-        
+    keyboardToolbar = [RFKeyboardToolbar toolbarViewWithButtons:@[exampleButton]];
+    _textView.inputAccessoryView = keyboardToolbar;
+    _textView.delegate = self;
+    
     [self.view addSubview:_textView];
+}
+
+- (void)textViewDidChange:(UITextView *)textView
+{
+    // get unique words in field
+    NSSet *words = [NSSet setWithArray:[textView.text.lowercaseString componentsSeparatedByCharactersInSet:[NSCharacterSet whitespaceAndNewlineCharacterSet]]];
+    
+    // dictionary of existing words => buttons
+    NSDictionary *oldButtons = [NSDictionary dictionaryWithObjects:keyboardToolbar.buttons forKeys:[keyboardToolbar.buttons valueForKey:@"title"]];
+    
+    // create new buttons
+    NSMutableArray *newButtons = [NSMutableArray arrayWithCapacity:words.count];
+    for (__strong NSString *word in words) {
+        word = [word stringByTrimmingCharactersInSet:[NSCharacterSet punctuationCharacterSet]];
+        if (word.length == 0) continue;
+        // create or reuse button
+        RFToolbarButton *button = oldButtons[word];
+        if (button == nil) {
+            button = [RFToolbarButton buttonWithTitle:word];
+            [button addEventHandler:^{
+                [_textView insertText:word];
+            } forControlEvents:UIControlEventTouchUpInside];
+        }
+        [newButtons addObject:button];
+    }
+    
+    [newButtons sortUsingComparator:^NSComparisonResult(id obj1, id obj2) {
+        return [[obj1 title] compare:[obj2 title]];
+    }];
+    [keyboardToolbar setButtons:newButtons animated:YES];
 }
 
 @end
