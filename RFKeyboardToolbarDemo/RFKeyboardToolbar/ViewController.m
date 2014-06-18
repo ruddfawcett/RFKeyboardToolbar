c0b55803084b105c898ba91eb0281d97e2cd9b7e
Jesús A. Álvarez <zydeco@namedfork.net>
2014-06-18 23:34:11 +0200

    example: order word buttons by frequency

--- RFKeyboardToolbarDemo/RFKeyboardToolbar/ViewController.m
+++ RFKeyboardToolbarDemo/RFKeyboardToolbar/ViewController.m
@@ -16,6 +16,25 @@
 
 @end
 
+@implementation UITextView (InsertWord)
+
+- (void)insertWord:(NSString*)word
+{
+    // insert a word into the field, adding a space before and/or after it as necessary
+    NSCharacterSet *whitespace = [NSCharacterSet whitespaceAndNewlineCharacterSet];
+    NSRange selectedRange = self.selectedRange;
+    NSString *text = self.text;
+    BOOL hasSpaceBefore = selectedRange.location == 0 || [whitespace characterIsMember:[text characterAtIndex:selectedRange.location-1]];
+    BOOL hasSpaceAfter = selectedRange.location+selectedRange.length == text.length || [whitespace characterIsMember:[text characterAtIndex:selectedRange.location+selectedRange.length]];
+    
+    NSMutableString *wordText = word.mutableCopy;
+    if (!hasSpaceBefore) [wordText insertString:@" " atIndex:0];
+    if (!hasSpaceAfter) [wordText appendString:@" "];
+    [self insertText:wordText];
+}
+
+@end
+
 @implementation ViewController
 {
     RFKeyboardToolbar *keyboardToolbar;
@@ -44,30 +63,38 @@
 
 - (void)textViewDidChange:(UITextView *)textView
 {
-    // get unique words in field
-    NSSet *words = [NSSet setWithArray:[textView.text.lowercaseString componentsSeparatedByCharactersInSet:[NSCharacterSet whitespaceAndNewlineCharacterSet]]];
+    // count words
+    NSArray *allWords = [textView.text.lowercaseString componentsSeparatedByCharactersInSet:[NSCharacterSet whitespaceAndNewlineCharacterSet]];
+    NSCountedSet *words = [NSCountedSet new];
+    for (__strong NSString *word in allWords) {
+        word = [word stringByTrimmingCharactersInSet:[NSCharacterSet punctuationCharacterSet]];
+        if (word.length == 0) continue;
+        [words addObject:word];
+    }
     
     // dictionary of existing words => buttons
     NSDictionary *oldButtons = [NSDictionary dictionaryWithObjects:keyboardToolbar.buttons forKeys:[keyboardToolbar.buttons valueForKey:@"title"]];
     
     // create new buttons
     NSMutableArray *newButtons = [NSMutableArray arrayWithCapacity:words.count];
-    for (__strong NSString *word in words) {
-        word = [word stringByTrimmingCharactersInSet:[NSCharacterSet punctuationCharacterSet]];
-        if (word.length == 0) continue;
+    for (NSString *word in words) {
         // create or reuse button
         RFToolbarButton *button = oldButtons[word];
         if (button == nil) {
             button = [RFToolbarButton buttonWithTitle:word];
             [button addEventHandler:^{
-                [_textView insertText:word];
+                [_textView insertWord:word];
             } forControlEvents:UIControlEventTouchUpInside];
         }
         [newButtons addObject:button];
     }
     
+    // sort by frequency and alphabetically
     [newButtons sortUsingComparator:^NSComparisonResult(id obj1, id obj2) {
-        return [[obj1 title] compare:[obj2 title]];
+        NSUInteger count1 = [words countForObject:[obj1 title]];
+        NSUInteger count2 = [words countForObject:[obj2 title]];
+        if (count1 == count2) return [[obj1 title] compare:[obj2 title]];
+        return count1 > count2 ? NSOrderedAscending : NSOrderedDescending;
     }];
     [keyboardToolbar setButtons:newButtons animated:YES];
 }
